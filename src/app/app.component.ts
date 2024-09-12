0496aee8ab7ac9f13583a7357ffeac41a6c81673
vyshnav <vyshnavkramesh@gmail.com>
2024-09-12 15:14:41 +0530

    experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from "./contact/contact.component";
+import { ExperienceComponent } from './experience/experience.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent],
+  imports: [RouterOutlet, ContactComponent,ExperienceComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Experience',component:ExperienceComponent}
+];
