549ddd186b61d165574daac2db48fe8c78227da3
Nandana Boban <nandanaboban03@gmail.com>
2024-09-12 13:11:59 +0530

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
